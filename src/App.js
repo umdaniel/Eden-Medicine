3c3d7544804c95c63106da4fa050535b6d4ffbd7
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-29 19:13:27 -0400

    Skeleton code and navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navbar from './components/Navbar';
+import GlobalStyle from './globalStyles';
+
+function App() {
+  return (
+    <>
+    <GlobalStyle />
+    <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/data/MenuData.js
@@ -0,0 +1,5 @@
+export const menuData = [
+    { title: 'About', link: '/about'},
+    { title: 'Products', link: '/products'},
+    { title: 'New', link: '/new'},
+]
